8e26ac107a0ed0353eda4c11bceed44dcf182b47
dgknca <dgknca@gmail.com>
2020-10-02 14:15:05 +0300

    index.js file place changed

---
+++ index.js
@@ -0,0 +1,7 @@
+import MultiSplitPane from './src/components/MultiSplitPane.vue'
+import Pane from './src/components/Pane.vue'
+
+export {
+    MultiSplitPane,
+    Pane
+}
--- src/index.js
+++
@@ -1,4 +0,0 @@
-import MultiSplitPane from './components/MultiSplitPane.vue'
-import Pane from './components/Pane.vue'
-
-export { MultiSplitPane, Pane }
