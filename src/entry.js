919e481621f7a7533b6cd466f59ef7ddb9d2e4e6
dgknca <dgknca@gmail.com>
2020-11-27 20:27:09 +0300

    browser dist

--- src/entry.js
+++ src/entry.js
@@ -1,37 +1,37 @@
 // Import vue components
-import * as components from '@/lib-components/index.js';
+import * as components from '@/lib-components/index.js'
 
 // install function executed by Vue.use()
 const install = function installVueMultiSplitPane(Vue) {
-  if (install.installed) return;
-  install.installed = true;
+  if (install.installed) return
+  install.installed = true
   Object.entries(components).forEach(([componentName, component]) => {
-    Vue.component(componentName, component);
-  });
-};
+    Vue.component(componentName, component)
+  })
+}
 
 // Create module definition for Vue.use()
 const plugin = {
-  install,
-};
+  install
+}
 
 // To auto-install on non-es builds, when vue is found
 // eslint-disable-next-line no-redeclare
 /* global window, global */
 if ('false' === process.env.ES_BUILD) {
-  let GlobalVue = null;
+  let GlobalVue = null
   if (typeof window !== 'undefined') {
-    GlobalVue = window.Vue;
+    GlobalVue = window.Vue
   } else if (typeof global !== 'undefined') {
-    GlobalVue = global.Vue;
+    GlobalVue = global.Vue
   }
   if (GlobalVue) {
-    GlobalVue.use(plugin);
+    GlobalVue.use(plugin)
   }
 }
 // Default export is library as a whole, registered via Vue.use()
-export default plugin;
+export default plugin
 
 // To allow individual component use, export components
 // each can be registered via Vue.component()
-export * from '@/lib-components/index.js';
+export * from '@/lib-components/index.js'
--- src/lib-components/index.js
+++ src/lib-components/index.js
@@ -1,7 +1,4 @@
 import MultiSplitPane from './components/MultiSplitPane.vue'
 import Pane from './components/Pane.vue'
 
-export {
-    MultiSplitPane,
-    Pane
-}
+export { MultiSplitPane, Pane }
